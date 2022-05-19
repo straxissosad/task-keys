6723976d2fa8219fcb45e7d33fd059bac0f7ea65
straxissosad <skrytyya@gmail.com>
2022-05-20 00:49:08 +0500

    keys

--- src/Keys.tsx
+++ src/Keys.tsx
@@ -1,5 +1,64 @@
 import { IItem } from './index';
+import { useState, useEffect } from 'react';
 
 export function Keys(props: { initialData: IItem[]; sorting: 'ASC' | 'DESC' }) {
-    return <div></div>;
+    const [isClicked, setClicked] = useState(false);
+    const [sort, setSorting] = useState(props.sorting);
+    const [id, setId] = useState(-1);
+    const [str, setStr] = useState('');
+    const [arr, setArr] = useState(props.initialData);
+
+    useEffect(() => {
+        if (props.sorting === 'ASC') {
+            setArr(arr.sort((a, b) => a.id - b.id));
+            setSorting(props.sorting);
+        }
+        if (props.sorting === 'DESC') {
+            setArr(arr.sort((a, b) => b.id - a.id));
+            setSorting(props.sorting);
+        }
+    }, [props.sorting]);
+
+    return (
+        <div>
+            {arr.map((item) => {
+                if (!isClicked || item.id !== id)
+                    return (
+                        <div
+                            key={item.id}
+                            onClick={() => {
+                                setClicked(true);
+                                setId(item.id);
+                            }}
+                        >
+                            {item.name}
+                        </div>
+                    );
+
+                return (
+                    <input
+                        defaultValue={item.name}
+                        key={item.id}
+                        type="text"
+                        onChange={(event) => setStr(event.target.value)}
+                        onKeyDown={(event) => {
+                            if (event.key === 'Escape') {
+                                setClicked(false);
+                            }
+                            if (event.key === 'Enter') {
+                                arr.map((elem) => {
+                                    if (elem.id === item.id) {
+                                        elem.name = str;
+                                    }
+                                });
+                                setArr(arr);
+                                setClicked(false);
+                            }
+                        }}
+                    />
+                );
+            })}
+        </div>
+    );
 }
+
